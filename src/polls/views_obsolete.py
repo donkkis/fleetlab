f554b8a19841c96d0aca96caa7b22f4b1864aacd
donkkis <panu.aho@gmail.com>
2020-02-23 15:30:25 +0200

    part 5 out of 7 done

--- src/polls/views_obsolete.py
+++ src/polls/views_obsolete.py
@@ -5,38 +5,31 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.urls import reverse
 from django.views import generic
-from django.utils import timezone
 from .models import Choice, Question
 
 
 # Create your views here.
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.filter(pub_date__lte=timezone.now()
-                                       ).order_by('-pub_date')[:5]
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-
-    def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+def detail(request, question_id):
+    # try:
+    #    question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #    raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
